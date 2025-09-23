c545930c1c7ddce471be2852578285998fb235e4
Adriner Andrade <adriner_andrade@hotmail.com>
2025-09-23 08:59:17 -0300

    feat: Creating C instruction assembler

--- src/Domain/Parsing/CInstructionParser.hs
+++ src/Domain/Parsing/CInstructionParser.hs
@@ -4,7 +4,7 @@ import Domain.Model.Parsing
 import Data.Bits ( (.|.) )
 
 parseCInstruction :: String -> String -> String -> Either ParserError String
-parseCInstruction dest cmp jmp = fmap concat (sequenceA [translateDest dest, translateComputation cmp, translateJmp jmp])
+parseCInstruction dest cmp jmp = fmap (("111" ++) . concat) (sequenceA [translateDest dest, translateComputation cmp, translateJmp jmp])
 
 translateDest :: String -> Either ParserError String
 translateDest [] = Right "000"
@@ -30,20 +30,72 @@ orLists :: [Int] -> [Int] -> [Int]
 orLists = zipWith (.|.)
 
 translateComputation :: String -> Either ParserError String
-translateComputation "0" = Right "0101010" -- a=0
-translateComputation "1" = Right "0111111" -- a=0
-translateComputation "D" = Right "0001100" -- a=0
+translateComputation "0" = Right "0101010"
+translateComputation "1" = Right "0111111"
+translateComputation "D" = Right "0001100"
 translateComputation "A" = Right "0110000" -- a=0
 translateComputation "M" = Right "1110000" -- a=1
+
+-- NumericNegateCmp
 translateComputation ('-':cs) = case cs of
-  "1" -> Right "0111010" -- a=0
-  "D" -> Right "0001111" -- a=0
+  "1" -> Right "0111010"
+  "D" -> Right "0001111"
   "A" -> Right "0110011" -- a=0
   "M" -> Right "1110011" -- a=1
-  neg  -> Left (ParserError $ "Invalid negate instruction: -" ++ neg)
+  neg  -> Left (ParserError $ "Invalid numeric negate instruction: -" ++ neg)
+
+-- LogicNegateCmp
+translateComputation ('!':cs) = case cs of
+  "D" -> Right "0001101"
+  "A" -> Right "0110001" -- a=0
+  "M" -> Right "1110001" -- a=1
+  neg  -> Left (ParserError $ "Invalid logic negate instruction: !" ++ neg)
+
+-- D computations
+translateComputation s@('D':op) = case op of
+  ('+':r) -> case r of
+    "1" -> Right "0011111"
+    "A" -> Right "0000010" -- a=0
+    "M" -> Right "1000010" -- a=1
+    _  -> Left (ParserError $ "Invalid sum for D register: -" ++ r)
+  ('-':r) -> case r of
+    "1" -> Right "0001110"
+    "A" -> Right "0010011" -- a=0
+    "M" -> Right "1010011" -- a=1
+    _  -> Left (ParserError $ "Invalid subtraction for D register: -" ++ r)
+  ('&':r) -> case r of
+    "A" -> Right "0000000" -- a=0
+    "M" -> Right "1000000" -- a=1
+    _  -> Left (ParserError $ "Invalid logical AND for D register: -" ++ r)
+  ('|':r) -> case r of
+    "A" -> Right "0010101" -- a=0
+    "M" -> Right "1010101" -- a=1
+    _  -> Left (ParserError $ "Invalid logical AND for D register: -" ++ r)
+
+  _  -> Left (ParserError $ "Invalid D operation: " ++ s)
+
+-- A computations
+translateComputation s@('A':op) = translateAddressComputation s "A" 0 op
+
+-- M computations
+translateComputation s@('M':op) = translateAddressComputation s "M" 0 op
 
 -- error
 translateComputation cs     = Left (ParserError $ "Invalid computation instruction: " ++ cs)
+
+translateAddressComputation :: String -> String -> Int -> String -> Either ParserError String
+translateAddressComputation s register bitValue op = case op of
+  "+1" -> Right $ addressInstruction "110111" 
+  ('-':r) -> case r of
+    "1" -> Right $ addressInstruction "110010" 
+    "D" -> Right $ addressInstruction "000111" 
+    _  -> Left (ParserError $ "Invalid subtraction for " ++ register ++ " register: -" ++ r)
+
+  _  -> Left (ParserError $ "Invalid " ++ register ++ " operation: " ++ s)
+  where
+    addressInstruction :: String -> String
+    addressInstruction a = show bitValue ++ a
+
 
 translateJmp :: String -> Either ParserError String
 translateJmp [] = Right "000"
